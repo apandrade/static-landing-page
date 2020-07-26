a96475d010fd97f8d82c8350515858ba63e754f7
Andre Andrade <andre.andrade@valtech.com>
2020-07-26 18:46:41 -0300

    Menu and logo components

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,24 +1,95 @@
 <template>
-  <svg class="NuxtLogo" width="245" height="180" viewBox="0 0 452 342" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M139 330l-1-2c-2-4-2-8-1-13H29L189 31l67 121 22-16-67-121c-1-2-9-14-22-14-6 0-15 2-22 15L5 303c-1 3-8 16-2 27 4 6 10 12 24 12h136c-14 0-21-6-24-12z"
-      fill="#00C58E"
-    />
-    <path
-      d="M447 304L317 70c-2-2-9-15-22-15-6 0-15 3-22 15l-17 28v54l39-67 129 230h-49a23 23 0 0 1-2 14l-1 1c-6 11-21 12-23 12h76c3 0 17-1 24-12 3-5 5-14-2-26z"
-      fill="#108775"
-    />
-    <path
-      d="M376 330v-1l1-2c1-4 2-8 1-12l-4-12-102-178-15-27h-1l-15 27-102 178-4 12a24 24 0 0 0 2 15c4 6 10 12 24 12h190c3 0 18-1 25-12zM256 152l93 163H163l93-163z"
-      fill="#2F495E"
-    />
-  </svg>
+  <div class="logo" :class="'logo--'+position">
+    <a href="/" :style="style">
+      <img v-if="hasImage" :src="logoImage"/>
+      <span v-if="hasText">{{text}}</span>
+    </a>
+  </div>
 </template>
 
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+    fontFamily: {
+      type: String,
+    },
+    fontColor:{
+      type: String,
+    },
+    position:{
+      type: String,
+      default: 'left',
+    },
+  },
+  data() {
+    return {
+      logoImage: this.url,
+      logoText: this.text || 'SiteLogo',
+      logoFont: this.fontFamily || 'Arial, Helvetica, sans-serif',
+      logoFontColor: this.fontColor || '#F5A506',
+    }
+  },
+  computed: {
+    hasImage() {
+      return !!this.logoImage;
+    },
+    hasText() {
+      return !!this.logoText;
+    },
+    style() {
+      return `font-family: ${this.logoFont}; color: ${this.logoFontColor};`;
+    }
+  }
+}
+</script>
+
 <style>
-.NuxtLogo {
-  animation: 1s appear;
-  margin: auto;
+.logo {
+  position: absolute;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  animation: appear 1s;
+  width: 150px;
+}
+
+.logo a {
+  vertical-align: top;
+  display: inline-block;
+  text-align: center;
+  text-decoration: none;
+  width: 100%;
+  height: 100%;
+}
+
+.logo img {
+  width: 100%;
+  height: 100%;
+}
+
+.logo span {
+  display: block;
+}
+
+.logo--right {
+  right: 0;
+}
+
+.logo--left {
+  left: 0;
+}
+
+.logo--center {
+  left: 0;
+  right: 0;
+  margin-right: auto;
+  margin-left: auto;
 }
 
 @keyframes appear {
---
+++ components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="menu" :style="style">
+        <slot name="logo"></slot>
+    </nav>
+</template>
+<script>
+export default {
+    props: {
+        bgColor: {
+            type: String,
+            default: 'transparent',
+        }
+    },
+    computed: {
+        style() {
+            return `background-color: ${this.bgColor}`;
+        }
+    }
+}
+</script>
+<style>
+.menu {
+    position: relative;
+    width: 95%;
+    height: 100px;
+    margin: 0 auto;
+}
+@media ( min-width: 768px ) {
+    .menu{
+        width: 80%;
+    }
+}
+</style>
