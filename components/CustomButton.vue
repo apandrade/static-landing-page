bff45618395ac11667dff2d7691166f337c1a058
Andre Andrade <andre.andrade@valtech.com>
2020-07-29 22:14:29 -0300

    Add CustomButton component

---
+++ components/CustomButton.vue
@@ -0,0 +1,83 @@
+<template>
+    <button
+        :class="buttonType"
+        :disabled="buttonDisabled"
+        @click="buttonClick($event)">
+        {{buttonText}}
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            type: String,
+            default: 'success',
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+        url: {
+            type: String,
+            default: '',
+        },
+        text: {
+            type: String,
+            default: 'Clique Aqui',
+        }
+    },
+    data() {
+        return {
+            buttonType: this.type,
+            buttonDisabled: this.disabled,
+            buttonUrl: this.url,
+            buttonText: this.text,
+        }
+    },
+    computed: {
+        hasLink() {
+            return !!this.buttonUrl;
+        }
+    },
+    methods: {
+        buttonClick: function(e) {
+            this.$emit('click', e);
+            if(this.hasLink) {
+                var win = window.open(this.buttonUrl, '_blank');
+                win.focus();
+            }
+        }
+    }
+}
+</script>
+
+<style>
+button {
+  padding: 10px 20px;
+  border: 1px solid #ddd;
+  color: #333;
+  background-color:#fff;
+  border-radius: 4px;
+  font-size: 14px;
+  cursor: pointer;
+}
+button[disabled]{
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+button.danger {
+    background-color: #ff4949;
+    color: #fff;
+  }
+button.success {
+    background-color: #13ce66;
+    color: #fff;
+  }
+button.info {
+    background-color: #50bfff;
+    color: #fff;
+  }
+
+
+</style>
